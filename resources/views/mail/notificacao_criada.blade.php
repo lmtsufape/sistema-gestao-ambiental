2d7074ec8f0db85e3248b08fa063d4df0f9fd66d
Jose Fernando Mendes da Costa <98fernandocosta@gmail.com>
2021-10-07 17:15:40 -0300

    adiciona arquivos para enviar email da notificação

---
+++ resources/views/mail/notificacao_criada.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+<body>
+    <p style="color: black; font-family: 'Times New Roman', Times, serif;">
+        Foi criada uma notificação para a empresa {{ $empresa }} @if ($imagens), as imagens seguem em anexo.@endif
+    </p>
+    <div>{!! $texto !!}</div>
+    <p style="'Times New Roman', Times, serif; font-size: 12px;">
+        Atenciosamente, <br>
+        {{ config('app.name') }} <br>
+        Laboratório Multidisciplinar de Tecnologias Sociais<br>
+        Universidade Federal do Agreste de Pernambuco
+    </p>
+</body>
+</html>
