e9bf5a2a398b2f5dc0a56c9a51e64d0ceae6a53b
KelwinJonas <kelwinjonas@gmail.com>
2021-12-15 15:30:02 -0300

    Views de emissao de licenca

--- resources/views/licenca/create.blade.php
+++ resources/views/licenca/create.blade.php
@@ -1,21 +1,22 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Emitir licença') }}
-        </h2>
-    </x-slot>
-
     <div class="container" style="padding-top: 5rem; padding-bottom: 8rem;">
         <div class="form-row justify-content-center">
             <div class="col-md-10">
+                <div class="form-row">
+                    <div class="col-md-8">
+                        <h4 class="card-title">Emitir licença</h4>
+                        <h6 class="card-subtitle mb-2 text-muted">Programação > Emitir licença</h6>
+                    </div>
+                    <div class="col-md-4" style="text-align: right">
+                        <a title="Voltar" href="{{route('visitas.index')}}">
+                            <img class="icon-licenciamento btn-voltar" src="{{asset('img/back-svgrepo-com.svg')}}" alt="Icone de voltar">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-10">
                 <div class="card" style="width: 100%;">
                     <div class="card-body">
-                        <div class="form-row">
-                            <div class="col-md-8">
-                                <h5 class="card-title">Emitir licença</h5>
-                                <h6 class="card-subtitle mb-2 text-muted">Programação > Emitir licença</h6>
-                            </div>
-                        </div>
                         <form method="POST" id="emitir-licenca-form" action="{{route('licenca.store')}}" enctype="multipart/form-data">
                             @csrf
                             <input type="hidden" name="visita" id="visita" value="{{$visita->id}}">
--- resources/views/licenca/show.blade.php
+++ resources/views/licenca/show.blade.php
@@ -1,21 +1,22 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Visualizar licença
-        </h2>
-    </x-slot>
-
     <div class="container" style="padding-top: 5rem; padding-bottom: 8rem;">
         <div class="form-row justify-content-center">
             <div class="col-md-10">
+                <div class="form-row">
+                    <div class="col-md-8">
+                        <h4 class="card-title">Licença com nº de referência {{$licenca->protocolo}}</h4>
+                        <h6 class="card-subtitle mb-2 text-muted">Programação > Visualizar licença</h6>
+                    </div>
+                    <div class="col-md-4" style="text-align: right">
+                        <a title="Voltar" href="{{route('visitas.index')}}">
+                            <img class="icon-licenciamento btn-voltar" src="{{asset('img/back-svgrepo-com.svg')}}" alt="Icone de voltar">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-10">
                 <div class="card" style="width: 100%;">
                     <div class="card-body">
-                        <div class="form-row">
-                            <div class="col-md-8">
-                                <h5 class="card-title">Licença com nº de referência {{$licenca->protocolo}}</h5>
-                                <h6 class="card-subtitle mb-2 text-muted">Programação > Visualizar licença</h6>
-                            </div>
-                        </div>
                         <div class="form-row">
                             <div class="col-md-12">
                                 <iframe src="{{asset('storage/'.$licenca->caminho)}}" frameborder="0" width="100%" height="500px"></iframe>
