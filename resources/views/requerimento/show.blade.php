76cc7bbbed4567cc10cac2216c705020f075a332
KelwinJonas <kelwinjonas@gmail.com>
2021-12-17 09:57:53 -0300

    Ajustes

--- resources/views/requerimento/show.blade.php
+++ resources/views/requerimento/show.blade.php
@@ -42,31 +42,27 @@
                             </div>
                             <div class="col-md-2">
                                 @can('isSecretario', \App\Models\User::class)
-                                    <div class="btn-group align-items-center">                                        
-                                        @if ($requerimento->documentos->count() > 0)
-                                            <a href="{{route('requerimento.documentacao', $requerimento->id)}}" style="cursor: pointer; margin-left: 2px; margin-right: 2px;"><img width="30" src="{{asset('img/documents-svgrepo-com.svg')}}"  alt="Analisar documentos" title="Analisar documentos"></a>
-                                            <a class="btn" data-toggle="modal" data-target="#documentos-edit" style="cursor: pointer; margin-left: 2px; margin-right: 2px;"><img width="30" src="{{asset('img/documents-transference-symbol-svgrepo-com.svg')}}"  alt="Editar documentos" title="Editar documentos"></a>
-                                        @else
-                                            <a class="btn" data-toggle="modal" data-target="#documentos" style="cursor: pointer;"><img width="30" src="{{asset('img/add-documents-svgrepo-com.svg')}}"  alt="Requistar documentos" title="Requistar documentos"></a>
-                                        @endif
-                                        @if($requerimento->visitas->count() > 0)
-                                            <a  href="{{route('requerimento.visitas', ['id' => $requerimento])}}" style="cursor: pointer; margin-left: 2px;"><img width="30" src="{{asset('img/chat-svgrepo-com.svg')}}"  alt="Visitas a empresa" title="Visitas a empresa"></a>
-
-                                        @endif
-                                    </div>
+                                    @if ($requerimento->documentos->count() > 0)
+                                        <a href="{{route('requerimento.documentacao', $requerimento->id)}}"><img class="icon-licenciamento" src="{{asset('img/documents-svgrepo-com.svg')}}"  alt="Analisar documentos" title="Analisar documentos"></a>
+                                        <a class="btn" data-toggle="modal" data-target="#documentos-edit" ><img class="icon-licenciamento" src="{{asset('img/documents-transference-symbol-svgrepo-com.svg')}}"  alt="Editar documentos" title="Editar documentos"></a>
+                                    @else
+                                        <a class="btn" data-toggle="modal" data-target="#documentos"><img class="icon-licenciamento" src="{{asset('img/add-documents-svgrepo-com.svg')}}"  alt="Requistar documentos" title="Requistar documentos"></a>
+                                    @endif
+                                    @if($requerimento->visitas->count() > 0)
+                                        <a  href="{{route('requerimento.visitas', ['id' => $requerimento])}}"><img class="icon-licenciamento" src="{{asset('img/chat-svgrepo-com.svg')}}"  alt="Visitas a empresa" title="Visitas a empresa"></a>
+                                    @endif
                                 @endcan
                                 @can('isAnalista', \App\Models\User::class)
-                                    <div class="btn-group align-items-center">
+                                    @if ($requerimento->documentos->count() > 0)
+                                        <a href="{{route('requerimento.documentacao', $requerimento->id)}}"><img class="icon-licenciamento" src="{{asset('img/documents-svgrepo-com.svg')}}"  alt="Analisar documentos" title="Analisar documentos"></a>
+                                    @endif
+                                    @can('isProtocolista', \App\Models\User::class)
+                                        <a class="btn" data-toggle="modal" data-target="#documentos"><img class="icon-licenciamento" src="{{asset('img/add-documents-svgrepo-com.svg')}}"  alt="Requistar documentos" title="Requistar documentos"></a>
+                                        <a  href="{{route('requerimentos.editar.empresa', $requerimento->id)}}"><img class="icon-licenciamento" src="{{asset('img/building-svgrepo-com.svg')}}"  alt="Editar empresa" title="Editar Informações da Empresa/Serviço"></a>
                                         @if ($requerimento->documentos->count() > 0)
-                                            <a href="{{route('requerimento.documentacao', $requerimento->id)}}" style="cursor: pointer; margin-left: 2px; margin-right: 2px;"><img width="30" src="{{asset('img/documents-svgrepo-com.svg')}}"  alt="Analisar documentos" title="Analisar documentos"></a>
-                                        @else
-                                            <a class="btn" data-toggle="modal" data-target="#documentos" style="cursor: pointer;"><img width="30" src="{{asset('img/add-documents-svgrepo-com.svg')}}"  alt="Requistar documentos" title="Requistar documentos"></a>
+                                            <a class="btn" data-toggle="modal" data-target="#documentos-edit"><img class="icon-licenciamento" src="{{asset('img/documents-transference-symbol-svgrepo-com.svg')}}"  alt="Editar documentos" title="Editar documentos"></a>
                                         @endif
-                                        @can('isProtocolista', \App\Models\User::class)
-                                            <a class="btn" data-toggle="modal" data-target="#documentos-edit" style="cursor: pointer; margin-left: 2px; margin-right: 2px;"><img width="30" src="{{asset('img/documents-transference-symbol-svgrepo-com.svg')}}"  alt="Editar documentos" title="Editar documentos"></a>
-                                            <a  href="{{route('requerimentos.editar.empresa', $requerimento->id)}}" style="cursor: pointer; margin-left: 2px; margin-right: 2px;"><img width="30" src="{{asset('img/building-svgrepo-com.svg')}}"  alt="Editar empresa" title="Editar Informações da Empresa/Serviço"></a>
-                                        @endcan
-                                    </div>
+                                    @endcan
                                 @endcan
                             </div>
                         </div>
