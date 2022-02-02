4a646227630689ce1ed37a7ecb97120fc243a8e1
Carlos André de Almeida Cavalcante <carlos.andre1270@gmail.com>
2022-02-02 10:08:34 -0300

    adicionando model noticia

---
+++ database/migrations/2022_02_01_144028_create_noticias_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNoticiasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('noticias', function (Blueprint $table) {
+            $table->id();
+            $table->string('imagem_principal');
+            $table->string('titulo');
+            $table->longText('texto');
+            $table->string('link');
+            $table->boolean('publicada')->default(false);
+            $table->unsignedBigInteger('autor_id');
+            $table->timestamps();
+
+            $table->foreign('autor_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('noticias');
+    }
+}
