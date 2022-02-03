7ab43b8475886791bbadc696908124af13b7c858
KelwinJonas <kelwinjonas@gmail.com>
2022-02-03 13:05:07 -0300

    Algumas legendas restantes

--- resources/views/documento/index.blade.php
+++ resources/views/documento/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="container" style="padding-top: 5rem; padding-bottom: 8rem;">
         <div class="form-row justify-content-center">
-            <div class="col-md-10">
+            <div class="col-md-9">
                 <div class="form-row">
                     <div class="col-md-8">
                         <h4 class="card-title">Documentos cadastrados no sistema</h4>
@@ -12,8 +12,6 @@
                         </a>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-10">
                 <div class="card card-borda-esquerda" style="width: 100%;">
                     <div class="card-body">
                         <div div class="form-row">
@@ -63,6 +61,47 @@
                             </tbody>
                         </table>
                     </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="col-md-12 shadow-sm p-2 px-3" style="background-color: #f8f9fa; border-radius: 00.5rem; margin-top: 2.6rem;">
+                    <div style="font-size: 21px;" class="tituloModal">
+                        Legenda
+                    </div>
+                    <ul class="list-group list-unstyled">
+                        <li>
+                            <div title="Adicionar documento" class="d-flex align-items-center my-1 pt-0 pb-1" style="border-bottom:solid 2px #e0e0e0;">
+                                <img class="aling-middle" style="border-radius: 50%;" width="20" src="{{asset('img/Grupo 1666.svg')}}" alt="Icone de adicionar documento">
+                                <div style="font-size: 15px;" class="aling-middle mx-3">
+                                    Adicionar documento
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div title="Visualizar documento" class="d-flex align-items-center my-1 pt-0 pb-1" style="border-bottom:solid 2px #e0e0e0;">
+                                <img class="aling-middle" width="20" src="{{asset('img/eye-svgrepo-com.svg')}}" alt="Visualizar documento">
+                                <div style="font-size: 15px;" class="aling-middle mx-3">
+                                    Visualizar documento
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div title="Editar documento" class="d-flex align-items-center my-1 pt-0 pb-1" style="border-bottom:solid 2px #e0e0e0;">
+                                <img class="aling-middle" width="20" src="{{asset('img/edit-svgrepo-com.svg')}}" alt="Editar documento">
+                                <div style="font-size: 15px;" class="aling-middle mx-3">
+                                    Editar documento
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div title="Deletar documento" class="d-flex align-items-center my-1 pt-0 pb-1" style="border-bottom:solid 2px #e0e0e0;">
+                                <img class="aling-middle" width="20" src="{{asset('img/trash-svgrepo-com.svg')}}" alt="Deletar documento">
+                                <div style="font-size: 15px;" class="aling-middle mx-3">
+                                    Deletar documento
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="container" style="padding-top: 5rem; padding-bottom: 8rem;">
         <div class="form-row justify-content-center">
-            <div class="col-md-10">
+            <div class="col-md-9">
                 <div class="form-row">
                     <div class="col-md-8">
                         <h4 class="card-title">Usuários cadastrados no sistema</h4>
@@ -12,8 +12,6 @@
                         </a>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-10">
                 <div class="card card-borda-esquerda" style="width: 100%;">
                     <div class="card-body">
                         <div div class="form-row">
@@ -50,6 +48,31 @@
                                 </tbody>
                         </table>
                     </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="col-md-12 shadow-sm p-2 px-3" style="background-color: #f8f9fa; border-radius: 00.5rem; margin-top: 2.6rem;">
+                    <div style="font-size: 21px;" class="tituloModal">
+                        Legenda
+                    </div>
+                    <ul class="list-group list-unstyled">
+                        <li>
+                            <div title="Adicionar usuário" class="d-flex align-items-center my-1 pt-0 pb-1" style="border-bottom:solid 2px #e0e0e0;">
+                                <img class="aling-middle" style="border-radius: 50%;" width="20" src="{{asset('img/Grupo 1666.svg')}}" alt="Icone de adicionar usuário">
+                                <div style="font-size: 15px;" class="aling-middle mx-3">
+                                    Adicionar usuário
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div title="Deletar usuário" class="d-flex align-items-center my-1 pt-0 pb-1" style="border-bottom:solid 2px #e0e0e0;">
+                                <img class="aling-middle" width="20" src="{{asset('img/trash-svgrepo-com.svg')}}" alt="Deletar usuário">
+                                <div style="font-size: 15px;" class="aling-middle mx-3">
+                                    Deletar usuário
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
