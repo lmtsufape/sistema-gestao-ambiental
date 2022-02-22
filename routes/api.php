a0b0cb390e1aff6cf9fa1f7ac7591af0a1646727
Jose Fernando Mendes da Costa <98fernandocosta@gmail.com>
2022-02-22 16:11:39 -0300

    rota do token e de visitas

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WebServiceCaixa\XMLCoderController;
 
@@ -15,8 +18,26 @@ use App\Http\Controllers\WebServiceCaixa\XMLCoderController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->get('/visitas', function (Request $request) {
+    return $request->user()->visitas()->with('requerimento', 'denuncia', 'solicitacao_poda')->get()->toArray();
+});
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->where('role', User::ROLE_ENUM['analista'])->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken($request->device_name)->plainTextToken;
 });
 
 Route::post('/teste/file', [XMLCoderController::class, 'teste'])->name('api.teste');
