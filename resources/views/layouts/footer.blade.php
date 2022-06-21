db578fd28fc6b4ca768f207aa88811ebb4ee116a
Jose Fernando Mendes da Costa <98fernandocosta@gmail.com>
2022-06-21 16:10:16 -0300

    ajustes footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
     <div id="appRodape">
         <div class="container">
             <div class="row">
-                <div class="col-md-3" style="margin-top: 40px;">
+                <div class="col-md-4" style="margin-top: 40px;">
                     <div class="row" style="text-align: center" >
                         <div class="col-md-6" >
                             <a href="https://garanhuns.pe.gov.br/" target="_blank" style="text-align: center">
@@ -14,40 +14,33 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-3" style="margin-top: 40px;">
-                    <div class="row" style="text-align: center">
-                        <div class="col-md-12">
-                            <a href="#">
-                                <img class="img" src="{{asset('img/logo.svg')}}" alt="Sistema de gestão ambiental" style="width: 240px; height: auto; display:inline;">
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <div class="row" style="text-align: center">
                         <div class="col-md-12">
                             <label>Desenvolvido por:</label>
                         </div>
-                        <div class="col-md-6" style="padding-top: 10px; border-right: 1px #000000 solid;">
-                            <a href="http://ufape.edu.br/" target="_blank">
-                                <img class="img" src="{{asset('img/ufape.png')}}" alt="Logo da UFAPE" style="display:inline;" />
-                            </a>
+                        <div class="col-md-6 border-md-right" style="padding-top: 10px;">
+                            <div class="row justify-center justify-content-md-end mr-md-2">
+                                <a href="http://ufape.edu.br/" target="_blank">
+                                    <img class="img " src="{{asset('img/ufape.svg')}}" alt="Logo da UFAPE" style="display:inline; height: 80px;" />
+                                </a>
+                            </div>
                         </div>
                         <div class="col-md-6" style="margin-top: 20px;">
                             <a href="http://lmts.ufape.edu.br/" target="_blank">
-                                <img class="img" src="{{asset('img/lmts.png')}}" alt="Logo do LMTS" style="display:inline;"/>
+                                <img class="img " src="{{asset('img/lmts.svg')}}" alt="Logo do LMTS" style="display:inline; width: 176px;"/>
                             </a>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <div class="row" style="text-align: center">
                         <div class="col-md-12">
                             <label>Redes sociais do LMTS:</label>
                         </div>
                         <div class="col-md-12">
-                            <a href="https://instagram.com/lmts_ufape?utm_medium=copy_link" target="_blank"><img class="img" class="img-rede-social" src="{{asset('img/Icon awesome-instagram.png')}}" alt="Instagram do LMTS" style="height: 80%; display:inline;"/></a>
-                            <a href="https://www.facebook.com/LMTSUFAPE" target="_blank"><img class="img" class="img-rede-social" src="{{asset('img/Icon awesome-facebook.png')}}" alt="Facebook do LMTS" style="height: 80%; display:inline;"/></a>
+                            <a href="https://instagram.com/lmts_ufape?utm_medium=copy_link" target="_blank"><img class="img" class="img-rede-social" src="{{asset('img/ig-white.svg')}}" alt="Instagram do LMTS" style="height: 80%; display:inline;"/></a>
+                            <a href="https://www.facebook.com/LMTSUFAPE" target="_blank"><img class="img" class="img-rede-social" src="{{asset('img/fb-white.svg')}}" alt="Facebook do LMTS" style="height: 80%; display:inline;"/></a>
                         </div>
                         <div class="col-md-12">
                             <a class="link-email" href="mailto:lmts@ufape.edu.br">lmts@ufape.edu.br</a>
