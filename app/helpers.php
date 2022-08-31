4399c0e863ebafc1e3ea7ab56acd1a00facadc95
Jose Fernando Mendes da Costa <98fernandocosta@gmail.com>
2022-08-31 11:27:09 -0300

    doc da funcao delete_file

--- app/helpers.php
+++ app/helpers.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Storage;
 
+/**
+ * Deleta o arquivo se o arquivo existe no disco especificado.
+ *
+ * @param string $path caminho do arquivo
+ * @param string $disk (opcional) disco onde o arquivo está localizado, por padrão usa o disco local
+ * @return void
+ */
 function delete_file($path, $disk = 'local'){
     if ($path != null && Storage::disk($disk)->exists($path)) {
         Storage::disk($disk)->delete($path);
