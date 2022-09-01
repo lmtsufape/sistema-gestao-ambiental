3c535d233198e048363117f9fec46f5dd6794a40
Jose Fernando Mendes da Costa <98fernandocosta@gmail.com>
2022-09-01 18:52:37 -0300

    dependencia do chartjs

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,5 +1,9 @@
+import Chart from 'chart.js/auto';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 window.$ = require('jquery')
 require('bootstrap');
 require('jquery-mask-plugin');
 require('bootstrap-select');
 window.Swal = require('sweetalert2');
+window.Chart = Chart
+window.ChartDataLabels = ChartDataLabels
