dcbe11762da195a382faba14b12e10131c8c60bb
Jose Fernando Mendes da Costa <98fernandocosta@gmail.com>
2022-09-21 11:44:44 -0300

    informa que a visita sera realizada ate a data

--- resources/views/mail/visita-alterada-poda.blade.php
+++ resources/views/mail/visita-alterada-poda.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
     <p style="color: black; font-family: 'Times New Roman', Times, serif;">
         A visita agendada para o endereço {{$poda->endereco->enderecoSimplificado()}} referente a uma solicitação de poda/supressão foi alterada para uma nova data. <br>
-        A visita ficou marcada para {{$data_marcada}}.
+        A visita será realizada até {{$data_marcada}}.
     </p>
     @lang('Regards'),<br>
     {{ config('app.name') }}<br>
--- resources/views/mail/visita-alterada-requerimento.blade.php
+++ resources/views/mail/visita-alterada-requerimento.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
     <p style="color: black; font-family: 'Times New Roman', Times, serif;">
         A visita agendada para a empresa {{$requerimento->empresa->nome}} do requerimento de {{$requerimento->tipoString()}} foi alterada para uma nova data. <br>
-        A visita ficou marcada para {{$data_marcada}}.
+        A visita será realizada até {{$data_marcada}}.
     </p>
     @lang('Regards'),<br>
     {{ config('app.name') }}<br>
