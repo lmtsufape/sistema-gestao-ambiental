b32a034909c917d7fd7ee83f0b29e5b4a803615a
unknown <antoniodurvalc@gmail.com>
2023-01-14 18:36:29 -0300

    Adição de campo de busca na parte de solicitações de poda

--- resources/views/solicitacoes/podas/index.blade.php
+++ resources/views/solicitacoes/podas/index.blade.php
@@ -26,6 +26,19 @@
                         </div>
                     @endif
                 </div>
+
+                <form action="{{route('podas.index', 'pendentes')}}" method="get">
+                    @csrf
+                    <div class="form-row mb-3">
+                        <div class="col-md-7">
+                            <input type="text" class="form-control w-100" name="buscar" placeholder="Digite o nome do requerente" value="{{ $busca }}">
+                        </div>
+                        <div class="col-md-3">
+                            <button type="submit" class="btn" style="background-color: #00883D; color: white;">Buscar</button>
+                        </div>
+                    </div>
+                </form>
+
                 <ul class="nav nav-tabs nav-tab-custom" id="myTab" role="tablist">
                     @can('isSecretario', \App\Models\User::class)
                         <li class="nav-item">
