ae14ad635f3bb3e9d55d9b4ddecf086083b63cd8
JoseDanielF <danielduartefilho.df@gmail.com>
2023-05-03 12:24:54 -0300

    Adicionado funcionalidade de alterar o requerente de uma empresa por outro cadastrado no sistema

--- app/Http/Livewire/EmpresasIndex.php
+++ app/Http/Livewire/EmpresasIndex.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Models\Empresa;
+use App\Models\Requerente;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -13,13 +14,14 @@ class EmpresasIndex extends Component
     public $search = '';
 
     public function render()
-    {
+    {   
+        $requerentes = Requerente::all();
         $search = preg_replace('/([0-9])/', '%$1', $this->search) . '%';
         $empresas = Empresa::where('nome', 'ilike', '%' . $this->search . '%')
             ->orWhere('cpf_cnpj', 'like', $search)
             ->orderBy('nome')
             ->paginate(20);
-        return view('livewire.empresas-index', ['empresas' => $empresas]);
+        return view('livewire.empresas-index', ['empresas' => $empresas, 'requerentes' => $requerentes]);
     }
 
     public function updatingSearch()
