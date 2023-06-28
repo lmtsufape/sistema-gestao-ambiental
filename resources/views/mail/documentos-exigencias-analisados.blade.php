458ac37e50b4d6370fed24b3893bb6b6cf1ea583
JoseDanielF <danielduartefilho.df@gmail.com>
2023-06-28 17:18:01 -0300

    Mail para envio de analise corrigido
    As comparações no foreach estavam erradas

--- resources/views/mail/documentos-exigencias-analisados.blade.php
+++ resources/views/mail/documentos-exigencias-analisados.blade.php
@@ -1,7 +1,3 @@
-@php
-    use App\Models\RequerimentoDocumento;
-@endphp
-
 @component('mail::message')
 
     <p style="color: black; font-family: 'Times New Roman', Times, serif;">
@@ -12,37 +8,51 @@
     </p>
     <ul style="color: black; font-family: 'Times New Roman', Times, serif;">
         @foreach ($documentos as $documento)
-            @foreach ($requerimento_documentos as $requerimento_documento)
-                @if ($requerimento_documento->documento_id == $documento->id)
-                    <li>
-                        <p style="color: black; font-family: 'Times New Roman', Times, serif;">
-                            {{$documento->nome}} - 
-                            @if($requerimento_documento->status == RequerimentoDocumento::STATUS_ENUM['aceito'])
-                                <span style="color: green;">Aceito</span>
+            @php
+                $requerimento_documento = $requerimento_documentos->where('requerimento_id', $requerimento->id)->where('documento_id', $documento->id)->first();
+            @endphp
+            @if ($requerimento_documento)
+                <li>
+                    @switch($requerimento_documento->status)
+                        @case(\App\Models\Checklist::STATUS_ENUM['aceito'])
+                            @if ($requerimento_documento->comentario_anexo)
+                                {{$documento->nome.' (aceito, comentário: '.$requerimento_documento->comentario_anexo.')'}}
                             @else
-                                <span style="color: red;">Recusado</span>
+                                {{$documento->nome.' (aceito)'}}
                             @endif
-                        </p>
-                        <p style="color: black; font-family: 'Times New Roman', Times, serif;"> Comentário: {{$requerimento_documento->comentario_anexo}}</p>
-                    </li>
-                @endif
-            @endforeach
+                            @break
+                        @case(\App\Models\Checklist::STATUS_ENUM['recusado'])
+                            @if ($requerimento_documento->comentario_anexo)
+                                {{$documento->nome.' (recusado, motivo:' .$requerimento_documento->comentario_anexo. ')'}}
+                            @else
+                                {{$documento->nome.' (recusado)'}}
+                            @endif
+                            @break
+                    @endswitch
+                </li>
+            @endif
         @endforeach
         @foreach ($requerimento_documentos as $requerimento_documento)
-                @if($requerimento_documento->nome_outro_documento != null)
-                    <li>
-                        <p style="color: black; font-family: 'Times New Roman', Times, serif;">
-                            {{$requerimento_documento->nome_outro_documento}} - 
-                            @if($requerimento_documento->status == RequerimentoDocumento::STATUS_ENUM['aceito'])
-                                <span style="color: green;">Aceito</span>
+            @if ($requerimento_documento->nome_outro_documento != null)
+                <li>
+                    @switch($requerimento_documento->status)
+                        @case(\App\Models\Checklist::STATUS_ENUM['aceito'])
+                            @if ($requerimento_documento->comentario_outro_documento)
+                                {{$requerimento_documento->nome_outro_documento.' (aceito, comentário: '.$requerimento_documento->comentario_outro_documento.')'}}
                             @else
-                                <span style="color: red;">Recusado</span>
+                                {{$requerimento_documento->nome_outro_documento.' (aceito)'}}
                             @endif
-                        </p>
-                        <p style="color: black; font-family: 'Times New Roman', Times, serif;"> Comentário: {{$requerimento_documento->comentario_outro_documento}}</p>
-                    </li>
-                    @break
-                @endif
+                            @break
+                        @case(\App\Models\Checklist::STATUS_ENUM['recusado'])
+                            @if ($requerimento_documento->comentario_outro_documento)
+                                {{$requerimento_documento->nome_outro_documento.' (recusado, motivo:' .$requerimento_documento->comentario_outro_documento. ')'}}
+                            @else
+                                {{$requerimento_documento->nome_outro_documento.' (recusado)'}}
+                            @endif
+                            @break
+                    @endswitch
+                </li>
+            @endif
         @endforeach
     </ul>
     @include('mail.footer')
