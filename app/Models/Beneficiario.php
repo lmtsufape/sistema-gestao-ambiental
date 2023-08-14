c930fd19b1190a485ba6380c72d69264ba6930fc
JoseDanielF <danielduartefilho.df@gmail.com>
2023-08-14 15:08:10 -0300

    Revert "Adicionado tipo de beneficiário e código na tabela de beneficiários"
    This reverts commit 24db819cb6c52d04e12f0409e71b68530436383f.

--- app/Models/Beneficiario.php
+++ app/Models/Beneficiario.php
@@ -6,16 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Beneficiario extends Model {
-    
     use HasFactory;
 
     protected $table = 'beneficiario';
-
-    public const ROLE_ENUM = [
-        'aracao' => 0,
-        'carro_pipa' => 1
-    ];
-
     protected $fillable = [
         'nome',
         'cpf',
@@ -25,12 +18,8 @@ class Beneficiario extends Model {
         'quantidade_pessoas',
         'observacao',
         'telefone_id',
-        'endereco_id',
-        'tipo_beneficiario',
-        'codigo'
+        'endereco_id'
     ];
-
-
 
     public function telefone() {
         return $this->belongsTo(Telefone::class);
@@ -49,8 +38,6 @@ class Beneficiario extends Model {
         $this->orgao_emissor = $input['orgao_emissor'];
         $this->quantidade_pessoas = $input['quantidade_pessoas'];
         $this->observacao = $input['observacao'];
-        $this->tipo_beneficiario =  $input['tipo_beneficiario'];
-        $this->codigo = $input['codigo'];
     }
 
 }
