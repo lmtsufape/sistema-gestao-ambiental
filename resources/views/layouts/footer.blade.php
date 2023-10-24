c449c8d55a26ced8f8ebded54e50d1b4aba26ca4
ellenmariadev <ellenmariadev@gmail.com>
2023-10-24 17:29:03 -0300

    fix: change lmts/ufape logo

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,48 +2,53 @@
     <div id="appRodape">
         <div class="container">
             <div class="row">
-                <div class="col-md-4">
-                    <div class="row" style="text-align: center" >
-                        <div class="col-md-12">
-                            <label>Realização:</label>
-                        </div>
-                        <div class="col-md-6" >
-                            <a href="https://garanhuns.pe.gov.br/" target="_blank" style="text-align: center">
-                                <img class="img" src="{{asset('img/prefeitura-garanhuns.svg')}}" alt="Prefeitura de Garanhuns" width="120px;" style="max-width: 120px; max-height: 120px; display:inline;" >
-                            </a>
-                        </div>
-                        <div class="col-md-6">
-                            <img class="img" src="{{asset('img/gus-logotipo.svg')}}" alt="Prefeitura de Garanhuns" width="120px;" style="max-width: 120px; max-height: 120px; display:inline;">
+                <div class="col-md-4" style="margin-bottom: 1rem">
+                    <div style="display: flex; place-content: center; flex-direction: column; text-align: center; align-items: center">
+                        <label>Realização:</label>
+                        <div style="display: flex; gap: 1rem; place-items: center">
+                            <div>
+                                <a href="https://garanhuns.pe.gov.br/" target="_blank" style="text-align: center">
+                                    <img class="img" src="{{ asset('img/prefeitura-garanhuns.svg') }}"
+                                        alt="Prefeitura de Garanhuns" width="100px;"
+                                        style="max-width: 100px; max-height: 100px; display:inline;">
+                                </a>
+                            </div>
+                            <div>
+                                <img class="img" src="{{ asset('img/gus-logotipo.svg') }}"
+                                    alt="Prefeitura de Garanhuns" width="100px;"
+                                    style="max-width: 100px; max-height: 100px; display:inline;">
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-4" style="display:flex; flex-direction: column; margin-inline: auto; margin-bottom: 1rem">
                     <div class="row" style="text-align: center">
-                        <div class="col-md-12">
-                            <label>Desenvolvido por:</label>
+                        <div class="col-md-10">
+                            <label style="text-align: center">Desenvolvido por:</label>
                         </div>
-                        <div class="col-md-6 border-md-right" style="padding-top: 10px;">
-                            <div class="row justify-center justify-content-md-end mr-md-2">
+                        <div class="col-md-9" style="padding-top: 10px;">
+                            <div class="row justify-center justify-content-md-end mr-md-0">
                                 <a href="http://ufape.edu.br/" target="_blank">
-                                    <img class="img " src="{{asset('img/ufape.svg')}}" alt="Logo da UFAPE" style="display:inline; height: 80px;" />
+                                    <img class="img " src="{{ asset('img/ufape-lmts.svg') }}" alt="Logo da UFAPE"
+                                        style="display:inline; height: 80px;" />
                                 </a>
                             </div>
                         </div>
-                        <div class="col-md-6" style="margin-top: 20px;">
-                            <a href="http://lmts.ufape.edu.br/" target="_blank">
-                                <img class="img " src="{{asset('img/lmts.svg')}}" alt="Logo do LMTS" style="display:inline; width: 176px;"/>
-                            </a>
-                        </div>
+
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-3" style="margin-bottom: 1rem">
                     <div class="row" style="text-align: center">
                         <div class="col-md-12">
                             <label>Redes sociais do LMTS:</label>
                         </div>
                         <div class="col-md-12">
-                            <a href="https://instagram.com/lmts_ufape?utm_medium=copy_link" target="_blank"><img class="img" class="img-rede-social" src="{{asset('img/ig-white.svg')}}" alt="Instagram do LMTS" style="height: 80%; display:inline;"/></a>
-                            <a href="https://www.facebook.com/LMTSUFAPE" target="_blank"><img class="img" class="img-rede-social" src="{{asset('img/fb-white.svg')}}" alt="Facebook do LMTS" style="height: 80%; display:inline;"/></a>
+                            <a href="https://instagram.com/lmts_ufape?utm_medium=copy_link" target="_blank"><img
+                                    class="img" class="img-rede-social" src="{{ asset('img/ig-white.svg') }}"
+                                    alt="Instagram do LMTS" style="height: 80%; display:inline;" /></a>
+                            <a href="https://www.facebook.com/LMTSUFAPE" target="_blank"><img class="img"
+                                    class="img-rede-social" src="{{ asset('img/fb-white.svg') }}" alt="Facebook do LMTS"
+                                    style="height: 80%; display:inline;" /></a>
                         </div>
                         <div class="col-md-12">
                             <a class="link-email" href="mailto:lmts@ufape.edu.br">lmts@ufape.edu.br</a>
